bebbf25cc16e8c43d31a10c4a2572901b406c453
vgorcinschi <vasigorc@gmail.com>
2017-05-31 22:55:29 -0400

    algorithms1_5_7 added support for RxScala

--- Chapter 1 Fundamentals/algorithms1_5_7/build.sbt
+++ Chapter 1 Fundamentals/algorithms1_5_7/build.sbt
@@ -5,4 +5,7 @@ name := "algorithms1_5_7"
 organization := "ch.epfl.scala"
 version := "1.0"
 libraryDependencies ++= Seq("org.scalatest" % "scalatest_2.12" % "3.0.1" % "test",
-			"org.scalacheck" %% "scalacheck" % "1.13.4" % "test")
+			"org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
+			"io.reactivex" % "rxscala_2.12" % "0.26.5")
+
+			
--- Chapter 1 Fundamentals/algorithms1_5_7/src/main/scala/ca/vgorcinschi/algorithms1_5_7/quickUnionUF/QuickUnionUF.scala
+++ Chapter 1 Fundamentals/algorithms1_5_7/src/main/scala/ca/vgorcinschi/algorithms1_5_7/quickUnionUF/QuickUnionUF.scala
@@ -1,5 +1,7 @@
 package ca.vgorcinschi.algorithms1_5_7.quickUnionUF
 
+import rx.lang.scala.Observable
+
 class QuickUnionUF(size: Int)extends AbstractUF{
   
   override val array = (0 to size).toArray
@@ -15,11 +17,18 @@ class QuickUnionUF(size: Int)extends AbstractUF{
   
   override def union(p: Int, q: Int) = {
     //add future or rx
-    val rootP = find(p)
-    val rootQ = find(q)
-    if(rootP != rootQ){
-      array(rootP) = rootQ
+    //val rootP = find(p)
+    //val rootQ = find(q)
+    //if(rootP != rootQ){
+       //array(rootP) = rootQ
+       //count -= 1
+  //}
+    val a = obsFind(p) zip obsFind(q) filter { case(p,q) => p != q }
+    a.subscribe((a)=>{
+      array(a._1) = a._2
       count -= 1
-    }
+    })
   }
+  
+  def obsFind(i:Int):Observable[Int] = Observable.just(i)
 }
