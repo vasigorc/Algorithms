0c7d6ae32220fecdf6b2b0679629e68e2592a183
Vasile Gorcinschi <vasigorc@gmail.com>
2018-07-28 21:20:47 -0400

    Finished + tested Ex2_2_17

--- Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms_2/Ex2_2_17.scala
+++ Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms_2/Ex2_2_17.scala
@@ -1,28 +1,40 @@
 package ca.vgorcinschi.algorithms_2
 
+import scala.collection.immutable.Queue
+import scala.util.Try
+
 /**
   * 2.2.17 Linked-list sort. Implement a natural mergesort for linked lists. (This is the
   * method of choice for sorting linked lists because it uses no extra space and is
   * guaranteed to be linearithmic.
   *
   * Implementation is inspired from Vladimir Kostyukov's Merge Sort implementation (not bottom-up in his case):
+  *
   * @see <a href="https://github.com/vkostyukov/scalacaster/blob/master/src/sort/MergeSort.scala">scalaster.Mergesort</a>
   */
 class Ex2_2_17 [T : Ordering]{
 
   def mergesort(list: List[T]):List[T] = {
     import Ordered._
+    implicit def queueToList(q: Queue[T]): List[T] = Option(q).fold(List.empty[T]) {_ toList}
+    case class TupleAndRemainder(tuple: (List[T], List[T]), remainder: List[T])
 
     def nextSubsequences(as: List[T]) : (List[T], List[T]) = {
       def loop(source: List[T], fs: List[T], ss: List[T]):(List[T], List[T]) = (source, fs, ss) match {
         case (first :: tail, Nil , Nil) => loop(tail, List(first), ss)
-        case (first :: tail, _, Nil) => if(first > fs.head) loop(tail, first :: fs, ss) else loop(tail, fs, List(first))
-        case (first :: tail, _, _) => if(first > ss.head) loop(tail, fs, first :: ss) else (fs, ss)
+        case (first :: tail, _, Nil) => if(first >= fs.head) loop(tail, first :: fs, ss) else loop(tail, fs, List(first))
+        case (first :: tail, _, _) => if(first >= ss.head) loop(tail, fs, first :: ss) else (fs.reverse, ss.reverse)
         case (Nil, _, Nil) => (fs.reverse, ss)
         case (Nil, _, _) => (fs.reverse, ss.reverse)
       }
 
       loop(as, Nil, Nil)
+    }
+
+    def tus(bs: List[T]): TupleAndRemainder = {
+      val (left, right) = nextSubsequences(bs)
+      val remainder = Try(bs.drop(left.size + right.size)).getOrElse(List.empty[T])
+      TupleAndRemainder((left, right), remainder)
     }
 
     def merge(left: List[T], right: List[T]): List[T] = (left, right) match {
@@ -32,20 +44,27 @@ class Ex2_2_17 [T : Ordering]{
     }
 
     def bottomUpSort(as: List[T]): List[T] = {
-      def loop(subsequences: (List[T], List[T]), unsorted: List[T], sorted: List[T]) :List[T] = {
+      def loop(subsequences: (List[T], List[T]), unsorted: List[T], sorted: Queue[T]) :List[T] = {
         if(subsequences._1.size == as.size) subsequences._1
         else{
           val mergedPart: List[T] = (merge _).tupled(subsequences)
           if(mergedPart.size == as.size) mergedPart
           else {
-            val newUnsorted = unsorted.drop(mergedPart.size)
-            loop(nextSubsequences(newUnsorted), newUnsorted, mergedPart)
+            unsorted match {
+              case Nil =>
+                val newSorted = merge(sorted, mergedPart)
+                val tupleAndRemainder: TupleAndRemainder = tus(newSorted)
+                loop(tupleAndRemainder.tuple, tupleAndRemainder.remainder, Queue.empty[T])
+              case xs =>
+                val tupleAndRemainder: TupleAndRemainder = tus(unsorted)
+                loop(tupleAndRemainder.tuple, tupleAndRemainder.remainder, Queue(merge(sorted, mergedPart): _*))
+            }
           }
         }
       }
 
       val firstTuple = nextSubsequences(as)
-      loop(firstTuple, as.drop(firstTuple._1.size + firstTuple._2.size), Nil)
+      loop(firstTuple, as.drop(firstTuple._1.size + firstTuple._2.size), Queue.empty[T])
     }
 
     bottomUpSort(list)
