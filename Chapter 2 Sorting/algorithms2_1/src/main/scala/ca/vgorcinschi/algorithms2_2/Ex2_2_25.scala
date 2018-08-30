300ba5fc81b54b8aa658a5acdc0126ac58e37e1b
Vasile Gorcinschi <vasigorc@gmail.com>
2018-08-30 17:23:45 -0400

    Ex2_2_25: fixed an important bug (for indices generation) and testing time to k benhcmarking

--- Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_2/Ex2_2_25.scala
+++ Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_2/Ex2_2_25.scala
@@ -1,9 +1,11 @@
 package ca.vgorcinschi.algorithms2_2
 
 import ca.vgorcinschi.algorithms2_1.BaseSort
+import org.scalameter
 
 import scala.annotation.tailrec
 import scala.reflect.ClassTag
+import math._
 
 /*
   Exercise 2.2.25 Multiway mergesort. Develop a mergesort implementation based on the idea of doing k-way merges (rather
@@ -22,7 +24,7 @@ class Ex2_2_25 [T : ClassTag : Ordering] extends BaseSort[T]{
       To partition the target array into equal segments we need to find the almost exact length of a segment, i.e: e/k | round
       Last segment will always be a little longer/shorter for an odd e or an odd k - which is ok
      */
-    val divisor: Int = math.round(e.toDouble/k).toInt
+    val divisor: Int = max(round(e.toDouble/k).toInt, 1)
 
     val indices: List[Int] = indicesGen(e, divisor) toList //generate list of "milestones"
     var previous = indices.head
@@ -57,4 +59,47 @@ class Ex2_2_25 [T : ClassTag : Ordering] extends BaseSort[T]{
   }
 
   override def sort(a: Array[T]): Array[T] = ???
+}
+
+object Ex2_2_25 extends App{
+
+  import scala.util.Random
+  import org.scalameter._
+
+  val instance = new Ex2_2_25[Int]
+  val useCase = (for (i <- 1 to 1000) yield Random.nextInt(1000)).toArray
+  val keys = Array(2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 18, 20, 50, 57, 80, 90)
+  val sortName = "Multiway Mergesort"
+
+  //duration test
+  keys.foreach{k =>
+    //ScalaMeter Warmer objects run the benhcmarked code until detecting steady state
+    val time = withWarmer(new scalameter.Warmer.Default) measure {
+      instance.ksort(useCase, k)
+    }
+    println(s"$sortName sorted Int array of size ${useCase.length} with $k way merge in $time")
+  }
+  /*
+    This yields:
+      Multiway Mergesort sorted Int array of size 1000 with 2 way merge in 3.953223 ms
+      Multiway Mergesort sorted Int array of size 1000 with 3 way merge in 5.565851 ms
+      Multiway Mergesort sorted Int array of size 1000 with 4 way merge in 6.751919 ms
+      Multiway Mergesort sorted Int array of size 1000 with 5 way merge in 3.880378 ms
+      Multiway Mergesort sorted Int array of size 1000 with 6 way merge in 3.551565 ms
+      Multiway Mergesort sorted Int array of size 1000 with 7 way merge in 4.243155 ms
+      Multiway Mergesort sorted Int array of size 1000 with 8 way merge in 4.840327 ms
+      Multiway Mergesort sorted Int array of size 1000 with 9 way merge in 4.74045 ms
+      Multiway Mergesort sorted Int array of size 1000 with 10 way merge in 4.150856 ms
+      Multiway Mergesort sorted Int array of size 1000 with 12 way merge in 10.185781 ms
+      Multiway Mergesort sorted Int array of size 1000 with 15 way merge in 6.783799 ms
+      Multiway Mergesort sorted Int array of size 1000 with 18 way merge in 7.92397 ms
+      Multiway Mergesort sorted Int array of size 1000 with 20 way merge in 8.585972 ms
+      Multiway Mergesort sorted Int array of size 1000 with 50 way merge in 38.804586 ms
+      Multiway Mergesort sorted Int array of size 1000 with 57 way merge in 45.286704 ms
+      Multiway Mergesort sorted Int array of size 1000 with 80 way merge in 60.434039 ms
+      Multiway Mergesort sorted Int array of size 1000 with 90 way merge in 62.598225 ms
+
+      Basically at 10-k algorithm starts to slow down, at k-s 80-90 it is ~ 20 times
+      longer then when merging with 2,5 or 6 way merge
+   */
 }
