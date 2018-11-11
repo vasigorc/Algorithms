3b80abd844d27d95736b4eb9b4f1c953864ba926
Vasile Gorcinschi <vasigorc@gmail.com>
2018-11-11 17:09:35 -0500

    Ex 2.3.6: fixing test set-up

--- Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_3/Ex2_3_6Props.scala
+++ Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_3/Ex2_3_6Props.scala
@@ -1,7 +1,7 @@
 package ca.vgorcinschi.algorithms2_3
 
-import ca.vgorcinschi.BaseProps
-import org.scalacheck.{Arbitrary, Gen, Prop}
+import ca.vgorcinschi.BasePropertyChecks
+import org.scalacheck.Gen
 import org.scalameter
 import org.scalameter.Measurer.MethodInvocationCount
 import org.scalameter.Quantity
@@ -9,39 +9,35 @@ import org.scalameter.execution.invocation.InvocationCountMatcher
 
 import scala.math.log
 
-class Ex2_3_6Props extends BaseProps {
+class Ex2_3_6Props extends BasePropertyChecks {
 
   //Ns for C_N
   private val GenNs: Gen[Int] = Gen.oneOf(100, 1000, 10000)
   private val quicksort = new QuickSort[Int]
 
-  //arbitrary array of Ints with values between Int.MinValue and Int.MaxValue
-  private val genIntArray: Gen[Array[Int]] = implicitly[Arbitrary[Array[Int]]].arbitrary
-
-  //with length.max == n
-  private def genArrayOfSize(n: Int): Gen[Array[Int]] = Gen.sized(n => genIntArray.suchThat(_.length == n))
-
   //mapping arrays to required lengths
-  private val finalTest: Gen[Array[Int]] = for {
-    n <- GenNs
-    arrayGen <- genArrayOfSize(n)
-  } yield arrayGen
-
+  private val finalTest: Gen[Array[Int]] = Gen.containerOf[Array, Int](GenNs)
 
   //tests execution
-  property("Number of array indices compare should be less or equal to 2NlnN") {
+  test("Number of array indices compare should be less or equal to 2NlnN") {
 
-    Prop.forAll(genIntArray) { x: Array[Int] =>
+    forAll(finalTest) { x: Array[Int] =>
 
-      val N = x.length
+      whenever(x.nonEmpty) {
 
-      val result: Quantity[Double] = scalameter.withMeasurer(MethodInvocationCount(
-        InvocationCountMatcher.forName("ca.vgorcinschi.algorithms2_1.BaseSort", "less"))
-        .map(v => v.copy(value = v.value.valuesIterator.sum))) measure {
-        quicksort.sort(x)
+        val N = x.length
+
+        info(s"array size is: $N")
+
+        val result: Quantity[Double] = scalameter.withMeasurer(MethodInvocationCount(
+          InvocationCountMatcher.forName("ca.vgorcinschi.algorithms2_1.BaseSort", "less"))
+          .map(v => v.copy(value = v.value.valuesIterator.sum))) measure {
+          quicksort.sort(x)
+        }
+        info(s"less was called ${result.value} times")
+        result.value <= 2 * N * log(N)
       }
-      info(s"${result.value}")
-      result.value <= 2*N*log(N)
+
     }
   }
 }
