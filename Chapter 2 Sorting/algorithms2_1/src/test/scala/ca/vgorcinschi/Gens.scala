19dc513dbcacccffd784c8ca35d19431c43ee1af
Vasile Gorcinschi <vasigorc@gmail.com>
2019-01-14 06:05:37 -0500

    Reduced arrays gens to ints from 0 to 100

--- Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/Gens.scala
+++ Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/Gens.scala
@@ -5,7 +5,5 @@ import org.scalacheck.Gen._
 
 trait Gens {
 
-  val arraysGen: Gen[Array[Int]] = containerOf[Array, Int](
-    chooseNum(Int.MinValue, Int.MaxValue) suchThat { _ < 100 }
-  ).suchThat(_.length < 50).suchThat(_.length > 0)
+  val minIntArraysGen: Gen[Array[Int]] = containerOf[Array, Int](Gen.chooseNum(0, 100))
 }
--- Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_3/MedianOf3PartitioningProps.scala
+++ Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_3/MedianOf3PartitioningProps.scala
@@ -1,18 +1,18 @@
 package ca.vgorcinschi.algorithms2_3
 
+import ca.vgorcinschi.Gens
 import org.scalatest.MustMatchers._
 import org.scalatest.WordSpec
 import org.scalatest.prop.PropertyChecks
 
-class MedianOf3PartitioningProps extends WordSpec with PropertyChecks {
+class MedianOf3PartitioningProps extends WordSpec with PropertyChecks with Gens {
 
   "sort method" must {
     "sort any Int array" in {
-      forAll { (l: List[Int]) =>
-        whenever(l.nonEmpty) {
-          val intArray: Array[Int] = l.toArray
-          val maybeSorted = new MedianOf3Partitioning[Int].sort(l.toArray)
-          maybeSorted must equal (intArray.sorted)
+      forAll (minIntArraysGen){ (a: Array[Int]) =>
+        whenever(a.nonEmpty) {
+          val maybeSorted = new MedianOf3Partitioning[Int].sort(a)
+          maybeSorted must equal (a.sorted)
         }
       }
     }
