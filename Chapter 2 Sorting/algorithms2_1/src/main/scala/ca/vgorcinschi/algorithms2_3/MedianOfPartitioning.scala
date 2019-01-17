0e181d6bd11dace607c069afa4fe8f5d0bd3fdb7
Vasile Gorcinschi <vasigorc@gmail.com>
2019-01-17 05:50:23 -0500

    Removed unused methods from MedianOfPartitioning trait

--- Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_3/MedianOfPartitioning.scala
+++ Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_3/MedianOfPartitioning.scala
@@ -3,31 +3,11 @@ package ca.vgorcinschi.algorithms2_3
 import ca.vgorcinschi.algorithms2_1.BaseSort
 import ca.vgorcinschi.algorithms2_3.QuickSort.{Left, Right, Scan}
 
-import scala.util.Random
-
 trait MedianOfPartitioning [T] {
 
   this: BaseSort[T] =>
 
   protected val MEDIAN_OF: Int
-
-  protected def getSample(a: Array[T], medianOf: Int): Array[T] = {
-    val sampleArrayOffset = Random.nextInt(a.length - medianOf)
-    a.slice(sampleArrayOffset, sampleArrayOffset + medianOf)
-  }
-
-  /**
-    * Mutate the main array to have at its head the smallest value from sample
-    * array and at its last index the biggest value from sample array
-    * @param a - target array
-    * @param sampleArray - size should be equal to MEDIAN_OF*/
-  def patchArray(a: Array[T], lo: Int, hi: Int, sampleArray: Array[T]): Unit = {
-    val (minVal, maxVal) = ((sampleArray.head, sampleArray.head) /: sampleArray) { case ((tempMin, tempMax), current) =>
-      (min(tempMin, current), max(tempMax, current))
-    }
-    exch(a, a.indexOf(minVal), lo)
-    exch(a, a.indexOf(maxVal), hi)
-  }
 
   protected def partition(a: Array[T], lo: Int, hi: Int, pivot: T): Int = {
     var (i, j) = (lo, hi + 1) //left and right scan indices
