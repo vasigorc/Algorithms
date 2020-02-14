da5db3c24850b764455c25dd7cc0a8181bd1a2da
Vasile Gorcinschi <vasigorc@gmail.com>
2020-02-13 22:45:24 -0500

    2.4.26: [WIP] MaxBeahviours

--- Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_4/MaxPQBehaviours.scala
+++ Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_4/MaxPQBehaviours.scala
@@ -2,15 +2,32 @@ package ca.vgorcinschi.algorithms2_4
 
 import ca.vgorcinschi.BaseSpec
 
+import scala.language.higherKinds
+
 trait MaxPQBehaviours[ImplLà[_] <: MaxPQ[_]] {
   self: BaseSpec =>
 
-  def delMax[Val](newMaxPQ: => ImplLà[Val], maxInt: Val)(implicit ev: Val <:< Ordered[Val]): Unit = {
+  def nonEmptyMaxPQ[Val](instance: => ImplLà[Val], sortedInput: List[Val])(implicit ev: Val <:< Ordered[Val]): Unit = {
+
+    val max = sortedInput.head
+
+    behavior of "size"
+
+    it should s"be equal to ${sortedInput.size}" in {
+      instance.size() shouldEqual sortedInput.size
+    }
+
+    behavior of "max"
+
+    it should s"return the expected $max" in {
+      instance.max() shouldEqual  max
+    }
 
     behavior of "delMax"
 
-    it should s"return the expected $maxInt" in {
-      newMaxPQ.delMax() shouldEqual  maxInt
+    it should "return the highest value until PQ is not empty" in {
+      sortedInput.reverse.foreach(n => n shouldEqual instance.delMax())
     }
+
   }
 }
