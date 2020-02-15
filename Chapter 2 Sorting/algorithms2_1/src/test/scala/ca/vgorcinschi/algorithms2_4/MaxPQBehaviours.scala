d685e5da1673e0832546b29cd058ca6e5861d9d4
Vasile Gorcinschi <vasigorc@gmail.com>
2020-02-15 12:36:36 -0500

    2.4.26: Replaced passing MaxPQs by name with passing them through Supplier

--- Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_4/MaxPQBehaviours.scala
+++ Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_4/MaxPQBehaviours.scala
@@ -5,7 +5,7 @@ import ca.vgorcinschi.BaseSpec
 trait MaxPQBehaviours[T <: Ordered[T], ImplLà <: MaxPQ[T]] {
   self: BaseSpec =>
 
-  def nonEmptyMaxPQ(instance: => ImplLà, sortedInput: List[T]): Unit = {
+  def nonEmptyMaxPQ(instanceSupplier: () => ImplLà, sortedInput: List[T]): Unit = {
 
     val reverseSorted = sortedInput.reverse
     val max = reverseSorted.head
@@ -14,22 +14,26 @@ trait MaxPQBehaviours[T <: Ordered[T], ImplLà <: MaxPQ[T]] {
     behavior of "size"
 
     it should s"be equal to ${sortedInput.size}" in {
+      val instance = instanceSupplier()
       instance.size() shouldEqual sortedInput.size
     }
 
     behavior of "max"
 
     it should s"return the expected $max" in {
+      val instance = instanceSupplier()
       instance.max() shouldEqual max
     }
 
     behavior of "delMax"
 
     it should "return the highest value until PQ is not empty" in {
+      val instance = instanceSupplier()
       reverseSorted.foreach(n => n shouldEqual instance.delMax())
     }
 
     it should "decrease size by 1" in {
+      val instance = instanceSupplier()
       instance.delMax()
       inputSize -1 shouldEqual instance.size()
     }
@@ -37,6 +41,7 @@ trait MaxPQBehaviours[T <: Ordered[T], ImplLà <: MaxPQ[T]] {
     behavior of "insert"
 
     it should "increase sie by 1" in {
+      val instance = instanceSupplier()
       // given remove one value
       val someValue = instance.delMax()
 
@@ -48,23 +53,26 @@ trait MaxPQBehaviours[T <: Ordered[T], ImplLà <: MaxPQ[T]] {
     }
   }
 
-  def emptyMaxPQ(instance: => ImplLà, sampleT: T): Unit = {
+  def emptyMaxPQ(instanceSupplier: () => ImplLà, sampleT: T): Unit = {
 
     behavior of "size"
 
     it should "be equal to 0" in {
+      val instance = instanceSupplier()
       instance.size() shouldEqual 0
     }
 
     behavior of "delMax"
 
     it should "throw NullPointerException when PQ is empty" in {
+      val instance = instanceSupplier()
       the [NullPointerException] thrownBy instance.delMax()
     }
 
     behavior of "insert"
 
     it should "increase size by 1" in {
+      val instance = instanceSupplier()
       instance.insert(sampleT)
       instance.size() shouldEqual 1
     }
