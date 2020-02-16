771b2c7923902c91edaee8b1429decf59900efd4
Vasile Gorcinschi <vasigorc@gmail.com>
2020-02-15 23:07:32 -0500

    2.4.26: Finished set-up of BaseMaxPQSpec

--- Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_4/BaseMaxPQSpec.scala
+++ Chapter 2 Sorting/algorithms2_1/src/test/scala/ca/vgorcinschi/algorithms2_4/BaseMaxPQSpec.scala
@@ -8,12 +8,27 @@ trait BaseMaxPQSpec extends BaseSpec with MaxPQBehaviours {
   type IntMaXPQ <: MaxPQ[Int]
   type DoubleMaxPQ <: MaxPQ[Double]
 
-  def charMaxPQ: CharMaXPQ
+  def nonEmptyCharMaxPQ: CharMaXPQ
+  def emptyCharMaxPQ: CharMaXPQ
   val sortedCharsList: List[Char] = List[Char]('C', 'b', 'd', 'a', 'z').sorted
 
-  def intMaxPQ: IntMaXPQ
+  def nonEmptyIntMaxPQ: IntMaXPQ
+  def emptyIntMaxPQ: IntMaXPQ
+  val sortedIntList: List[Int] = List[Int](213, 3, 4, 343 ,8, 9, 100).sorted
 
-  def doubleMaxPQ: DoubleMaxPQ
+  def nonEmptyDoubleMaxPQ: DoubleMaxPQ
+  def emptyDoubleMaxPQ: DoubleMaxPQ
+  val sortedDoubleList: List[Double] = List[Double](345d, 76d, 45.67, 1,23, 766d, 90.00).sorted
 
-  it should behave like nonEmptyMaxPQ(() => charMaxPQ, sortedCharsList)
+  behavior of "non-empty PQ"
+
+  it should behave like nonEmptyMaxPQ(() => nonEmptyCharMaxPQ, sortedCharsList)
+  it should behave like nonEmptyMaxPQ(() => nonEmptyIntMaxPQ, sortedIntList)
+  it should behave like nonEmptyMaxPQ(() => nonEmptyDoubleMaxPQ, sortedDoubleList)
+
+  behavior of "empty PQ"
+
+  it should behave like emptyMaxPQ(() => emptyCharMaxPQ, 'A')
+  it should behave like emptyMaxPQ(() => emptyIntMaxPQ, 1)
+  it should behave like emptyMaxPQ(() => emptyDoubleMaxPQ, 1d)
 }
