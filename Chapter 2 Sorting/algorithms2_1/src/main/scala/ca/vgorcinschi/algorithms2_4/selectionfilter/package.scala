f1d9e3f84d9b652fe7bc2b0a551e5239900d92ce
Vasile Gorcinschi <vasigorc@gmail.com>
2021-01-15 19:07:24 -0500

    Add MutableArrayMaxPQRunner

---
+++ Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_4/selectionfilter/package.scala
@@ -0,0 +1,14 @@
+package ca.vgorcinschi.algorithms2_4
+
+import scala.reflect.ClassTag
+
+package object selectionfilter {
+  def arrayMaxPQFromRunner[R, I <: Stream[R], V](
+    input: I,
+    consumeOneF: ArrayMaxPQ[V] => R => Unit,
+    size: Int
+  )(implicit tag: ClassTag[V], cmp: Ordering[_ >: V]) = {
+    val pq = new ArrayMaxPQ[V](size)
+    input.foreach(r => consumeOneF(pq)(r))
+  }
+}
