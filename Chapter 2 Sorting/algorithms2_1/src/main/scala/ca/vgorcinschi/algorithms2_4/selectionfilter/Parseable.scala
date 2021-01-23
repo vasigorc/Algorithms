6c1a33bca89b4bca241f8ac53093f8d3b4121a11
Vasile Gorcinschi <vasigorc@gmail.com>
2021-01-23 15:33:25 -0500

    Remove extra memory usage in parseCoordinatePoint

--- Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_4/selectionfilter/Parseable.scala
+++ Chapter 2 Sorting/algorithms2_1/src/main/scala/ca/vgorcinschi/algorithms2_4/selectionfilter/Parseable.scala
@@ -2,26 +2,25 @@ package ca.vgorcinschi.algorithms2_4.selectionfilter
 
 import cats.implicits._
 
-/**
- * takes an input string and returns
- * either an instance of the required
- * type (Double, Int, Boolean, CoordinatePoint, etc.)
- * or the initially passed-in string
- *
- * @tparam T - required return type
- */
+/** takes an input string and returns
+  * either an instance of the required
+  * type (Double, Int, Boolean, CoordinatePoint, etc.)
+  * or the initially passed-in string
+  *
+  * @tparam T - required return type
+  */
 trait Parseable[T] {
   def parse(input: String): Either[String, T]
 }
 
-/**
- * `import Parseable._` before calling `InputParser.parse(input)`
- */
+/** `import Parseable._` before calling `InputParser.parse(input)`
+  */
 object Parseable {
   implicit val parseDouble: Parseable[Double] = new Parseable[Double] {
     override def parse(input: String): Either[String, Double] = {
       val simplifiedInput = input.replaceAll("[ \\n]", "").toLowerCase
-      try Right(simplifiedInput.toDouble) catch {
+      try Right(simplifiedInput.toDouble)
+      catch {
         case _: NumberFormatException =>
           Left(input)
       }
@@ -31,27 +30,37 @@ object Parseable {
   implicit val parseInt: Parseable[Int] = new Parseable[Int] {
     override def parse(input: String): Either[String, Int] = {
       val simplifiedInput = input.replaceAll("[ \\n]", "").toLowerCase
-      try Right(simplifiedInput.toInt) catch {
+      try Right(simplifiedInput.toInt)
+      catch {
         case _: NumberFormatException =>
           Left(input)
       }
     }
   }
 
-  implicit val parseCoordinatePoint: Parseable[CoordinatePoint] = new Parseable[CoordinatePoint] {
-    override def parse(input: String): Either[String, CoordinatePoint] = {
+  implicit val parseCoordinatePoint: Parseable[CoordinatePoint] =
+    (input: String) => {
       val simplifiedInput = input.replaceAll("[ \\n]", "").toLowerCase
-      val unparsedPoints: List[String] = simplifiedInput.split(",").toList
-      val eithers: List[Either[String, Double]] = unparsedPoints.map(parseDouble.parse)
-      val sequence: Either[String, List[Double]] = eithers.sequence
+      val sequence: Either[String, List[Double]] =
+        simplifiedInput.split(",")
+          .toList
+          .map(parseDouble.parse)
+          .sequence
       sequence match {
         case Left(value) => Left(value)
-        case Right(doublePoints) => Right(CoordinatePoint(doublePoints.head, doublePoints(1), doublePoints(2)))
+        case Right(doublePoints) =>
+          Right(
+            CoordinatePoint(
+              doublePoints.head,
+              doublePoints(1),
+              doublePoints(2)
+            )
+          )
       }
     }
-  }
 }
 
 object InputParser {
-  def parse[T](input: String)(implicit p: Parseable[T]): Either[String, T] = p.parse(input)
+  def parse[T](input: String)(implicit p: Parseable[T]): Either[String, T] =
+    p.parse(input)
 }
